284a0558f08657b9b1abbab11d9251375aee3572
Alim Midov <a.midov@tinkoff.ru>
2022-04-17 09:08:24 +0300

    refactoring Application test class

--- src/test/java/com/tcs/edu/Application.java
+++ src/test/java/com/tcs/edu/Application.java
@@ -1,11 +1,7 @@
 package com.tcs.edu;
 
-import com.tcs.edu.decorator.Severity;
-import com.tcs.edu.printer.ConsolePrinter;
 import com.tcs.edu.service.MessageService;
-
 import static com.tcs.edu.decorator.Severity.*;
-import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
 
 /**
  * Test case
@@ -16,29 +12,5 @@ class Application {
         MessageService.process(REGULAR, "Hello world!", "Hi!", "Hey!", "What's up!");
         MessageService.process(MINOR, "Hello world!", "Hi!", "Hey!", "What's up!");
         MessageService.process("Hello world!", "Hi!", "Hey!", "What's up!");
-
-//        MessageService.process(MAJOR, "Hello world!");
-//        MessageService.process(MINOR, "Hello world!");
-//        MessageService.process(MAJOR, "Hello world!");
-//        MessageService.process(MAJOR, "Hello world!");
-//        MessageService.process(REGULAR, "Hello world!");
-//        MessageService.process(MINOR, "Hello world!");
-//        MessageService.process(REGULAR, "Hello world!");
-//        MessageService.process(REGULAR, "Hello world!");
-//        MessageService.process(REGULAR, "Hello world!");
-//        MessageService.process(null, "Hello world!");
-//        MessageService.process("Hello world!");
-
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
-//        ConsolePrinter.print(decorate("Hello world!"));
     }
 }
