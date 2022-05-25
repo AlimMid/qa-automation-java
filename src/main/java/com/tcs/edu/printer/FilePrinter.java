d84ec9fe64051fd660401fcda8a226288573cd32
AlimMid <38467048+AlimMid@users.noreply.github.com>
2022-05-25 17:14:08 +0300

    feature (#12)
    add FilePrinter class
    add ValidatingService class
    ConsolePrinter: use abstract class ValidatingService
    OrderedDistinctedMessageService: use abstract class ValidatingService
    
    Co-authored-by: Alim Midov <a.midov@tinkoff.ru>

--- src/main/java/com/tcs/edu/printer/FilePrinter.java
+++ src/main/java/com/tcs/edu/printer/FilePrinter.java
@@ -3,19 +3,17 @@ package com.tcs.edu.printer;
 import com.tcs.edu.validator.ValidatingService;
 
 /**
- * @author Midov Alim
  * @version 1.0.0
- * @apiNote Класс com.tcs.edu.printer.ConsolePrinter предназначен для вывода информации на консоль
+ * @apiNote Класс com.tcs.edu.printer.FilePrinter предназначен для вывода информации в файл
  */
-public class ConsolePrinter extends ValidatingService implements Printer {
+public class FilePrinter extends ValidatingService implements Printer {
+
     /**
      * @param message тип String
      * @apiNote Метод предназначен для вывода сообщения передаваемого в качестве аргумента
      */
     @Override
     public void print(String message) {
-        if (isArgNotValid(message)) { return;}
-        System.out.println(message);
+        if (isArgNotValid(message)) {return;}
     }
-
 }
