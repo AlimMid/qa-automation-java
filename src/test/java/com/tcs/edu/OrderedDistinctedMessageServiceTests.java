26dff36870d8901da4de6a424196a4926b4f4dad
Alim Midov <a.midov@tinkoff.ru>
2022-06-05 12:47:39 +0300

    task12.3
    OrderedDistinctedMessageServiceTests: use nested tests

--- src/test/java/com/tcs/edu/OrderedDistinctedMessageServiceTests.java
+++ src/test/java/com/tcs/edu/OrderedDistinctedMessageServiceTests.java
@@ -8,11 +8,10 @@ import com.tcs.edu.repository.HashMapMessageRepository;
 import com.tcs.edu.service.MessageService;
 import com.tcs.edu.service.OrderedDistinctedMessageService;
 import com.tcs.edu.validator.LogException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import java.util.stream.Collectors;
+
 import static com.tcs.edu.decorator.Doubling.DISTINCT;
 import static com.tcs.edu.decorator.Doubling.DOUBLES;
 import static com.tcs.edu.decorator.MessageOrder.ASC;
@@ -22,6 +21,7 @@ import static com.tcs.edu.decorator.Severity.MINOR;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test Service")
 public class OrderedDistinctedMessageServiceTests {
     private MessageService messageService;
     private Message messageMin;
@@ -41,120 +41,130 @@ public class OrderedDistinctedMessageServiceTests {
         messageEmpty = new Message("");
     }
 
-    @Test
-    @Tag("negative")
-    @DisplayName("Проверка валидации, когда messageOrder = null")
-    public void checkValidationMessageOrderNull() {
-        Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(null, DOUBLES, messageErr));
-        assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
-    }
+    @Nested
+    @DisplayName("Проверка валидации")
+    class ValidationTests {
 
-    @Test
-    @Tag("negative")
-    @DisplayName("Проверка валидации, когда doubling = null")
-    public void checkValidationDoublingNull() {
-        Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(ASC, (Doubling) null, messageErr));
-        assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
-    }
+        @Test
+        @Tag("negative")
+        @DisplayName("Проверка валидации, когда messageOrder = null")
+        public void checkValidationMessageOrderNull() {
+            Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(null, DOUBLES, messageErr));
+            assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
+        }
 
-    @Test
-    @Tag("negative")
-    @DisplayName("Проверка валидации, когда doubling = null при незаданном messageOrder")
-    public void checkValidationDoublingNullWhenOrderEmpty() {
-        Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log((Doubling) null, messageErr));
-        assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
-    }
+        @Test
+        @Tag("negative")
+        @DisplayName("Проверка валидации, когда doubling = null")
+        public void checkValidationDoublingNull() {
+            Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(ASC, (Doubling) null, messageErr));
+            assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
+        }
 
-    @Test
-    @Tag("negative")
-    @DisplayName("Проверка валидации, когда messageOrder = null при незаданном doubling")
-    public void checkValidationOrderNullWhenDoublingEmpty() {
-        Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log((MessageOrder) null, messageErr));
-        assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
-    }
+        @Test
+        @Tag("negative")
+        @DisplayName("Проверка валидации, когда doubling = null при незаданном messageOrder")
+        public void checkValidationDoublingNullWhenOrderEmpty() {
+            Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log((Doubling) null, messageErr));
+            assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
+        }
 
-    @Test
-    @Tag("negative")
-    @DisplayName("Проверка валидации, когда в Messages все месседжи = null")
-    public void checkValidationMessagesNull() {
-        Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(ASC, DOUBLES, null, null));
-        assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
-    }
+        @Test
+        @Tag("negative")
+        @DisplayName("Проверка валидации, когда messageOrder = null при незаданном doubling")
+        public void checkValidationOrderNullWhenDoublingEmpty() {
+            Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log((MessageOrder) null, messageErr));
+            assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
+        }
 
-    @Test
-    @Tag("negative")
-    @DisplayName("Проверка валидации, когда в Messages не заданы")
-    public void checkValidationMessagesEmpty() {
-        Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(DESC, DOUBLES));
-        assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
-    }
+        @Test
+        @Tag("negative")
+        @DisplayName("Проверка валидации, когда в Messages все месседжи = null")
+        public void checkValidationMessagesNull() {
+            Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(ASC, DOUBLES, null, null));
+            assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
+        }
 
-    @Test
-    @Tag("negative")
-    @DisplayName("Проверка валидации, когда в Messages имеется message=''")
-    public void checkValidationMessagesContainEmptyMessage() {
-        Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(ASC, DOUBLES, messageErr, messageEmpty));
-        assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
-    }
+        @Test
+        @Tag("negative")
+        @DisplayName("Проверка валидации, когда в Messages не заданы")
+        public void checkValidationMessagesEmpty() {
+            Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(DESC, DOUBLES));
+            assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
+        }
 
-    @Test
-    @Tag("positive")
-    @DisplayName("Проверка, что при повторных вызовах мессаджи записываются в коллекцию корректно")
-    public void checkMessagesAfterDoubleLog() {
-        messageService.log(ASC, DOUBLES, messageMin);
-        messageService.log(ASC, DOUBLES, messageMaj);
-        var messages = messageService.findAll();
-        assertThat(messages).isNotNull().hasSize(2);
-        var messMin = messages.stream()
-                .filter(m -> m.getId().equals(messageMin.getId()))
-                .collect(Collectors.toList());
-        var messMaj = messages.stream()
-                .filter(m -> m.getId().equals(messageMaj.getId()))
-                .collect(Collectors.toList());
-        assertThat(messMin).hasSize(1);
-        assertThat(messMin.get(0).getSeverity()).isEqualTo(messageMin.getSeverity());
-        assertThat(messMin.get(0).getBody()).isEqualTo(messageMin.getBody());
-        assertThat(messMaj).hasSize(1);
-        assertThat(messMaj.get(0).getSeverity()).isEqualTo(messageMaj.getSeverity());
-        assertThat(messMaj.get(0).getBody()).isEqualTo(messageMaj.getBody());
-    }
-
-    @Test
-    @Tag("positive")
-    @DisplayName("Проверка, что при doubling = DOUBLES, одинаковые сообщения не дедуплицируются")
-    public void checkDoubleMessages() {
-        messageService.log(ASC, DOUBLES, messageMin, messageMin);
-        var messages = messageService.findAll();
-        assertThat(messages).isNotNull().hasSize(2);
-        for (var message : messages) {
-            assertThat(message.getSeverity()).isEqualTo(messageMin.getSeverity());
-            assertThat(message.getBody()).isEqualTo(messageMin.getBody());
+        @Test
+        @Tag("negative")
+        @DisplayName("Проверка валидации, когда в Messages имеется message=''")
+        public void checkValidationMessagesContainEmptyMessage() {
+            Throwable throwMessage = assertThrows(LogException.class, () -> messageService.log(ASC, DOUBLES, messageErr, messageEmpty));
+            assertThat(throwMessage.getMessage()).isEqualTo("notValidArgMessage");
         }
     }
 
-    @Test
-    @Tag("positive")
-    @DisplayName("Проверка, что при doubling = DISTINCT, одинаковые сообщения дедуплицируются")
-    public void checkDistinctMessages() {
-        messageService.log(ASC, DISTINCT, messageMin, messageMin);
-        var messages = messageService.findAll();
-        assertThat(messages).isNotNull().hasSize(1);
-        for (var message : messages) {
-            assertThat(message.getSeverity()).isEqualTo(messageMin.getSeverity());
-            assertThat(message.getBody()).isEqualTo(messageMin.getBody());
-        }
-    }
+    @Nested
+    @DisplayName("Проверка сервиса. Позитивные кейсы")
+    class PositiveCheckServiceTests {
 
-    @Test
-    @Tag("positive")
-    @DisplayName("Проверка, что при незаданном doubling, сообщения записываются без дедупликации")
-    public void checkDefaultDoubling() {
-        messageService.log(ASC, messageMin, messageMin);
-        var messages = messageService.findAll();
-        assertThat(messages).isNotNull().hasSize(2);
-        for (var message : messages) {
-            assertThat(message.getSeverity()).isEqualTo(messageMin.getSeverity());
-            assertThat(message.getBody()).isEqualTo(messageMin.getBody());
+        @Test
+        @Tag("positive")
+        @DisplayName("Проверка, что при повторных вызовах мессаджи записываются в коллекцию корректно")
+        public void checkMessagesAfterDoubleLog() {
+            messageService.log(ASC, DOUBLES, messageMin);
+            messageService.log(ASC, DOUBLES, messageMaj);
+            var messages = messageService.findAll();
+            assertThat(messages).isNotNull().hasSize(2);
+            var messMin = messages.stream()
+                    .filter(m -> m.getId().equals(messageMin.getId()))
+                    .collect(Collectors.toList());
+            var messMaj = messages.stream()
+                    .filter(m -> m.getId().equals(messageMaj.getId()))
+                    .collect(Collectors.toList());
+            assertThat(messMin).hasSize(1);
+            assertThat(messMin.get(0).getSeverity()).isEqualTo(messageMin.getSeverity());
+            assertThat(messMin.get(0).getBody()).isEqualTo(messageMin.getBody());
+            assertThat(messMaj).hasSize(1);
+            assertThat(messMaj.get(0).getSeverity()).isEqualTo(messageMaj.getSeverity());
+            assertThat(messMaj.get(0).getBody()).isEqualTo(messageMaj.getBody());
+        }
+
+        @Test
+        @Tag("positive")
+        @DisplayName("Проверка, что при doubling = DOUBLES, одинаковые сообщения не дедуплицируются")
+        public void checkDoubleMessages() {
+            messageService.log(ASC, DOUBLES, messageMin, messageMin);
+            var messages = messageService.findAll();
+            assertThat(messages).isNotNull().hasSize(2);
+            for (var message : messages) {
+                assertThat(message.getSeverity()).isEqualTo(messageMin.getSeverity());
+                assertThat(message.getBody()).isEqualTo(messageMin.getBody());
+            }
+        }
+
+        @Test
+        @Tag("positive")
+        @DisplayName("Проверка, что при doubling = DISTINCT, одинаковые сообщения дедуплицируются")
+        public void checkDistinctMessages() {
+            messageService.log(ASC, DISTINCT, messageMin, messageMin);
+            var messages = messageService.findAll();
+            assertThat(messages).isNotNull().hasSize(1);
+            for (var message : messages) {
+                assertThat(message.getSeverity()).isEqualTo(messageMin.getSeverity());
+                assertThat(message.getBody()).isEqualTo(messageMin.getBody());
+            }
+        }
+
+        @Test
+        @Tag("positive")
+        @DisplayName("Проверка, что при незаданном doubling, сообщения записываются без дедупликации")
+        public void checkDefaultDoubling() {
+            messageService.log(ASC, messageMin, messageMin);
+            var messages = messageService.findAll();
+            assertThat(messages).isNotNull().hasSize(2);
+            for (var message : messages) {
+                assertThat(message.getSeverity()).isEqualTo(messageMin.getSeverity());
+                assertThat(message.getBody()).isEqualTo(messageMin.getBody());
+            }
         }
     }
 }
