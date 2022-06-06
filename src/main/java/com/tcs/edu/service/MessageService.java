0f875320ecb3bd40f25605310d4c60d0f9292050
AlimMid <38467048+AlimMid@users.noreply.github.com>
2022-06-06 16:53:41 +0300

    Task11.2 (#20)
    * Task11.1
    
    MessageDecorator: use Message instead of String
    TimestampMessageDecorator: decorate and set body of the message
    Message: add UUID field
    add repository interface and implementation for saving messages
    OrderedDistinctedMessageService: refactoring, use repository instead of print
    
    * Task11.2 add findByPrimaryKey method
    
    * Task11.2 add test
    
    Co-authored-by: Alim Midov <a.midov@tinkoff.ru>
    Co-authored-by: Alexey <iNTEAM@users.noreply.github.com>

--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -4,10 +4,13 @@ import com.tcs.edu.decorator.Doubling;
 import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.domain.Message;
 
+import java.util.UUID;
+
 /**
  * Интерфейс процесса обработки и вывода сообщений
  */
 public interface MessageService {
+    Message findByPrimaryKey(UUID key);
     void log(MessageOrder messageOrder, Doubling doubling, Message... messages);
     void log(MessageOrder messageOrder, Message... messages);
     void log(Doubling doubling, Message... messages);
